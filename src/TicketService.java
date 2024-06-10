5d343518beb618f1da085959b14feda6538b660e
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2024-06-10 07:43:36 +0400

    JFB-1: Add ticket storage commit

---
+++ src/TicketService.java
@@ -0,0 +1,16 @@
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TicketService {
+    private List<Ticket> tickets;
+
+    public TicketService() {
+        this.tickets = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            tickets.add(new Ticket("ID" + i, "ConcertHall" + i, 100 + i, LocalDateTime.now(), false, 'A', 5.0, new BigDecimal("50.00")));
+        }
+    }
+}
